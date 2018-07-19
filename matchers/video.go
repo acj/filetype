27e279c801fe6a805e230f1a34cdc579c3aa9978
Adam Jensen <acjensen@gmail.com>
2018-07-19 14:44:42 -0400

    Handle mov block size of 0x20

--- matchers/video.go
+++ matchers/video.go
@@ -58,7 +58,7 @@ func Webm(buf []byte) bool {
 func Mov(buf []byte) bool {
 	return len(buf) > 7 &&
 		buf[0] == 0x0 && buf[1] == 0x0 &&
-		buf[2] == 0x0 && buf[3] == 0x14 &&
+		buf[2] == 0x0 && (buf[3] == 0x14 || buf[3] == 0x20) &&
 		buf[4] == 0x66 && buf[5] == 0x74 &&
 		buf[6] == 0x79 && buf[7] == 0x70
 }
